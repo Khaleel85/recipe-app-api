2e00a6b25968910346f5dc4800fc1d98980f1ee7
Khaleel <global-config@example.com>
2023-04-05 02:42:24 +0200

    Added advanced updating features to Ingredient API & fixed linting

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -356,6 +356,7 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertIn(ingredient2, recipe.ingredients.all())
         self.assertNotIn(ingredient1, recipe.ingredients.all())
+
     def test_clear_recipe_ingredients(self):
         """Test clearing a recipes ingredients"""
         ingredient = Ingredient.objects.create(user=self.user, name='Garlic')
@@ -368,6 +369,3 @@ class PrivateRecipeApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
-
-
-
